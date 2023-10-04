325500d3cf38cb1a4e20d952ce14cc591a69213a
onofrej <onofre.junior@stackspot.com>
2023-10-04 19:20:28 -0300

    feat: Initial commit - creating project structure

---
+++ KafkaCommitStrategyConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿var cancellationToken = new CancellationTokenSource().Token;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "host1:9092,host2:9092",
+    GroupId = "foo",
+    EnableAutoCommit = true,
+    EnableAutoOffsetStore = true,
+    AutoCommitIntervalMs = 5000,
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("");
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume(cancellationToken);
+
+        if (consumeResult is not null)
+        {
+            consumer.StoreOffset(consumeResult);
+        }
+    }
+}
